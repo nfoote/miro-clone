a103bd96d9127a73bdfdf1d241237c202401c4d4
Nick <nfoote@utas.edu.au>
2022-10-30 13:17:44 +1100

    Add socket to handle mouse moving and log the x and y coords on backend

--- backend/app.js
+++ backend/app.js
@@ -14,26 +14,53 @@ const io = socketIo(server, {
     }
 });
 
-let interval;
+// let interval;
 
-io.on("connection", (socket) => {
-  console.log("New client connected");
+// io.on("connection", (socket) => {
+//   console.log("New client connected");
 
-  if (interval) {
-    clearInterval(interval);
-  }
+//   if (interval) {
+//     clearInterval(interval);
+//   }
   
-  interval = setInterval(() => getApiAndEmit(socket), 1000);
+//   interval = setInterval(() => getApiAndEmit(socket), 1000);
+//   socket.on("disconnect", () => {
+//     console.log("Client disconnected");
+//     clearInterval(interval);
+//   });
+// });
+
+// const getApiAndEmit = socket => {
+//   const response = new Date();
+//   // Emitting a new message. Will be consumed by the client
+//   socket.emit("FromAPI", response);
+// };
+const clients = {};
+
+const addClient = socket => {
+  console.log("New client connected", socket.id);
+  clients[socket.id] = socket;
+};
+const removeClient = socket => {
+  console.log("Client disconnected", socket.id);
+  delete clients[socket.id]; // todo delete?
+};
+
+io.sockets.on("connection", socket => {
+  let id = socket.id;
+
+  addClient(socket);
+
+  socket.on("mousemove", data => {
+    console.log(data);
+    data.id = id;
+    socket.broadcast.emit("moving", data);
+  });
+
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
+    removeClient(socket);
+    socket.broadcast.emit("clientdisconnect", id);
   });
 });
-
-const getApiAndEmit = socket => {
-  const response = new Date();
-  // Emitting a new message. Will be consumed by the client
-  socket.emit("FromAPI", response);
-};
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
